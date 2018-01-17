1afe22dca1179a5571618386eaccd0e06df16966
bwzheng <zheng.biwen@puscene.com>
2018-01-17 11:40:37 +0800

    gen

--- src/mybatis-generator/gen.sh
+++ src/mybatis-generator/gen.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -x
-CLASSPATH=mysql-connector-java-6.0.6.jar:mapper-3.4.0.jar:mybatis-generator-core-1.3.5.jar
+CLASSPATH=mysql-connector-java-5.1.37.jar:mapper-3.3.9.jar:mybatis-generator-core-1.3.5.jar
 mainClassName=org.mybatis.generator.api.ShellRunner
-java -classpath "$CLASSPATH" "$mainClassName" -configfile generatorConfig.xml -overwrite
+java -classpath "$CLASSPATH" "$mainClassName" -configfile generatorConfig_win_zbw.xml -overwrite
